2dc7fe0886b402b4ddcf2ca3f289cb66fa72b04e
Yoshi Fu <yoshi.fu@student.uva.nl>
2022-07-07 11:26:22 +0200

    added docstrings

--- tests/test_minion_meister.py
+++ tests/test_minion_meister.py
@@ -6,7 +6,7 @@ Project:  Minion Meister Discord Bot
 Date:     July 24th 2022
 
 Summary:
-- Contains unit tets for the database.
+- Contains unit tests for the database.
 - [TODO]
 """
 
@@ -16,6 +16,7 @@ from minion_meister import MinionMeister
 
 
 def test_connection(mm: MinionMeister):
+    """ Test if the MinionMeister connection fixture works. """
     assert mm.database
     assert isinstance(mm.database, str)
     assert mm.database.endswith('.db')
@@ -23,12 +24,14 @@ def test_connection(mm: MinionMeister):
 
 @pytest.mark.asyncio
 async def test_add_user(mm: MinionMeister, mm_data: dict):
+    """ Test if a user is added to database. """
     await mm.add_user(mm_data['sid'], mm_data['uid'], mm_data['name'])
     assert await mm.is_user(mm_data['sid'], mm_data['uid'])
 
 
 @pytest.mark.asyncio
 async def test_add_user_error(mm: MinionMeister, mm_data: dict):
+    """ Test if adding the user again, gives InsertError. """
     try:
         await mm.add_user(mm_data['sid'], mm_data['uid'], mm_data['name'])
         assert False
@@ -38,12 +41,14 @@ async def test_add_user_error(mm: MinionMeister, mm_data: dict):
 
 @pytest.mark.asyncio
 async def test_select_winner(mm: MinionMeister, mm_data: dict):
+    """ Test if selecting a MinionMeister works. """
     uid = await mm.select_winner(mm_data['sid'])
     assert uid == mm_data['uid']
 
 
 @pytest.mark.asyncio
 async def test_show_participants(mm: MinionMeister, mm_data: dict):
+    """ Test if listing all participants works. """
     names = await mm.show_participants(mm_data['sid'])
     assert isinstance(names, list)
     assert isinstance(names[0], str)
@@ -52,6 +57,7 @@ async def test_show_participants(mm: MinionMeister, mm_data: dict):
 
 @pytest.mark.asyncio
 async def test_show_history(mm: MinionMeister, mm_data: dict):
+    """ Test if showing history works. """
     history = await mm.show_history(mm_data['sid'], mm_data['limit'])
     assert isinstance(history, tuple)
     assert len(history) == 2
@@ -65,6 +71,7 @@ async def test_show_history(mm: MinionMeister, mm_data: dict):
 
 @pytest.mark.asyncio
 async def test_show_count(mm: MinionMeister, mm_data: dict):
+    """ Test if showing counts works. """
     counts = await mm.show_count(mm_data['sid'])
     assert isinstance(counts, tuple)
     assert len(counts) == 2
@@ -79,6 +86,7 @@ async def test_show_count(mm: MinionMeister, mm_data: dict):
 
 @pytest.mark.asyncio
 async def test_insert_history(mm: MinionMeister, mm_data: dict):
+    """ Test if inserting history works. """
     await mm.insert_history(mm_data['sid'], mm_data['uid'], mm_data['date'])
     names = await mm.show_participants(mm_data['sid'])
     assert mm_data['name'] in names
@@ -91,6 +99,7 @@ async def test_insert_history(mm: MinionMeister, mm_data: dict):
 
 @pytest.mark.asyncio
 async def test_delete_history(mm: MinionMeister, mm_data: dict):
+    """ Test if deleting history works. """
     await mm.delete_history(mm_data['sid'], mm_data['uid'], mm_data['date'])
     _names, dates = await mm.show_history(mm_data['sid'], mm_data['limit'])
     assert not mm_data['date'] in dates
@@ -98,12 +107,14 @@ async def test_delete_history(mm: MinionMeister, mm_data: dict):
 
 @pytest.mark.asyncio
 async def test_admin_user(mm: MinionMeister, mm_data: dict):
+    """ Test if user can be given admin permissions. """
     await mm.admin_user(mm_data['sid'], mm_data['uid'], mm_data['name'])
     assert await mm.is_admin(mm_data['sid'], mm_data['uid'])
 
 
 @pytest.mark.asyncio
 async def test_admin_user_error(mm: MinionMeister, mm_data: dict):
+    """ Test if giving user admin permissions again, gives InsertError. """
     try:
         await mm.admin_user(mm_data['sid'], mm_data['uid'], mm_data['name'])
         assert False
@@ -113,6 +124,7 @@ async def test_admin_user_error(mm: MinionMeister, mm_data: dict):
 
 @pytest.mark.asyncio
 async def test_show_admins(mm: MinionMeister, mm_data: dict):
+    """ Test if listing all admins works. """
     names = await mm.show_admins(mm_data['sid'])
     assert isinstance(names, list)
     assert isinstance(names[0], str)
@@ -121,12 +133,14 @@ async def test_show_admins(mm: MinionMeister, mm_data: dict):
 
 @pytest.mark.asyncio
 async def test_unadmin_user(mm: MinionMeister, mm_data: dict):
+    """ Test if taking admin from user works. """
     await mm.unadmin_user(mm_data['sid'], mm_data['uid'], mm_data['name'])
     assert not await mm.is_admin(mm_data['sid'], mm_data['uid'])
 
 
 @pytest.mark.asyncio
 async def test_unadmin_user_error(mm: MinionMeister, mm_data: dict):
+    """ Test if taking admin from user again, gives DeleteError. """
     try:
         await mm.unadmin_user(mm_data['sid'], mm_data['uid'], mm_data['name'])
         assert False
@@ -136,12 +150,14 @@ async def test_unadmin_user_error(mm: MinionMeister, mm_data: dict):
 
 @pytest.mark.asyncio
 async def test_remove_user(mm: MinionMeister, mm_data: dict):
+    """ Test if removing user from database works. """
     await mm.remove_user(mm_data['sid'], mm_data['uid'], mm_data['name'])
     assert not await mm.is_user(mm_data['sid'], mm_data['uid'])
 
 
 @pytest.mark.asyncio
 async def test_remove_user_error(mm: MinionMeister, mm_data: dict):
+    """ Test if removing user from database again, gives DeleteError. """
     try:
         await mm.remove_user(mm_data['sid'], mm_data['uid'], mm_data['name'])
         assert False
